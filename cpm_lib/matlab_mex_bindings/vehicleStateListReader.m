a9524c643d1f2fe31c86c41bb1f6490e2afe2fb1
Leon Rabanus <rabanus@embedded.rwth-aachen.de>
2021-06-08 13:18:39 +0200

    Added how-to comment

--- cpm_lib/matlab_mex_bindings/vehicleStateListReader.m
+++ cpm_lib/matlab_mex_bindings/vehicleStateListReader.m
@@ -1,6 +1,14 @@
 %% This function makes sure that the called MEX file can run out-of-process
 % Except for errors or deletion, the process remains active in between calls
 % You can delete the process by calling delete vehicleStateListReader.m
+
+% To be able to call this file without errors, you have to replace Matlabs
+% own glibcxx with your system's one
+% cd /usr/local/MATLAB/.../sys/os/glnxa64
+% sudo mkdir old
+% sudo mv libstdc++.so.6* old
+% sudo ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 libstdc++.so.6 
+
 function result = vehicleStateListReader()
     persistent mh
     if ~(isa(mh,'matlab.mex.MexHost') && isvalid(mh))
