26e66e632dcbd385a336546dd3b0ee349cd9c64f
Leon Rabanus <rabanus@embedded.rwth-aachen.de>
2021-06-28 11:11:32 +0200

    ARM build: Minor bash change

--- cpm_lib/build_arm.bash
+++ cpm_lib/build_arm.bash
@@ -12,8 +12,12 @@ if [ ! -d "dds_idl_cpp" ]; then
     ./ddsgen.bash
 fi
 
+cd thirdparty/foonathan_memory_vendor/
+mkdir -p build
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../../../Toolchain.cmake -DFOONATHAN_MEMORY_BUILD_TOOLS=OFF -DFOONATHAN_MEMORY_BUILD_TESTS=OFF ..
 
-cd build_arm
+cd ../../../build_arm
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake -DBUILD_ARM=ON
 make -j$(nproc)
 cd ..
