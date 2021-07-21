c18c2df85fcc68b7dd529fb200585880e8ce9b3e
Leon Rabanus <rabanus@embedded.rwth-aachen.de>
2021-07-21 10:54:57 +0200

    Matlab scripts should now be more versatile

--- cpm_lib/matlab_mex_bindings/build_mex.bash
+++ cpm_lib/matlab_mex_bindings/build_mex.bash
@@ -12,30 +12,31 @@ if [[ -z "${MEX_PATH// }" ]]; then
     exit 0
 fi
 
+# OUTDATED: Update-alternatives is not flexible w.r.t. Ubuntu and Matlab versions (would need different GCC versions)
 # Check for sudo permissions
-if ! sudo true; then
-    warning_msg="WARNING: Without sudo, the Mex Matlab Bindings will not be built! "
-    warning_msg+="You may build them on your own (see build_mex.bash or README.md in matlab_mex_bindings / Confluence Doc. to see how) without using sudo, "
-    warning_msg+="which may mean that you cannot call update-alternatives to switch to a supported GCC version. "
-    warning_msg+="This may not actually be problematic, as MEX will only print a warning in that case."
-    echo $warning_msg
-    exit 0
-fi
+# if ! sudo true; then
+#     warning_msg="WARNING: Without sudo, the Mex Matlab Bindings will not be built! "
+#     warning_msg+="You may build them on your own (see build_mex.bash or README.md in matlab_mex_bindings / Confluence Doc. to see how) without using sudo, "
+#     warning_msg+="which may mean that you cannot call update-alternatives to switch to a supported GCC version. "
+#     warning_msg+="This may not actually be problematic, as MEX will only print a warning in that case."
+#     echo $warning_msg
+#     exit 0
+# fi
 
-# Switch to gcc-9, which is the newest version supported by Matlab as of June 2021
-# This only works if you have called the matlab_setup script before!
-GCC_ALTERNATIVES=$(update-alternatives --list gcc)
-GPP_ALTERNATIVES=$(update-alternatives --list g++)
-if [[ "$GCC_ALTERNATIVES" != *"gcc-9"* ]] || [[ "$GPP_ALTERNATIVES" != *"g++-9"* ]]
-then
-    echo "ERROR: Missing update-alternatives. Please call matlab_setup.sh before installing the MEX Bindings for Matlab. You can find the script in the top folder of the repository."
-    echo "Aborting..."
-    exit 0
-fi
+# # Switch to gcc-9, which is the newest version supported by Matlab as of June 2021
+# # This only works if you have called the matlab_setup script before!
+# GCC_ALTERNATIVES=$(update-alternatives --list gcc)
+# GPP_ALTERNATIVES=$(update-alternatives --list g++)
+# if [[ "$GCC_ALTERNATIVES" != *"gcc-9"* ]] || [[ "$GPP_ALTERNATIVES" != *"g++-9"* ]]
+# then
+#     echo "ERROR: Missing update-alternatives. Please call matlab_setup.sh before installing the MEX Bindings for Matlab. You can find the script in the top folder of the repository."
+#     echo "Aborting..."
+#     exit 0
+# fi
 
-# Checks done, now start
-sudo update-alternatives --set gcc /usr/bin/gcc-9
-sudo update-alternatives --set g++ /usr/bin/g++-9
+# # Checks done, now start
+# sudo update-alternatives --set gcc /usr/bin/gcc-9
+# sudo update-alternatives --set g++ /usr/bin/g++-9
 
 # Compile the MEX files
 echo "Now compiling your MEX files, given that mex is on the PATH and the cpm lib, FastDDS and FastCDR are installed..."
@@ -47,5 +48,5 @@ $MEX_PATH vehicle_command_trajectory_writer.cpp -L"$CPM_DIR/build/" -lcpm -I"$CP
 echo "Done"
 
 # Revert to your system's default gcc
-sudo update-alternatives --auto gcc
-sudo update-alternatives --auto g++
+# sudo update-alternatives --auto gcc
+# sudo update-alternatives --auto g++
--- matlab_setup.sh
+++ matlab_setup.sh
@@ -11,23 +11,34 @@ else
     real_user=$(whoami)
 fi
 
+# OUTDATED: Update-alternatives (depends too much on Ubuntu and Matlab version)
 # Install update-alternatives for GCC for MEX compilation (newer versions than v9 are not supported by Matlab as of June 2021)
-sudo apt install gcc-9 g++-9
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 10
+# sudo apt install gcc-9 g++-9
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 10
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 10
 # Note: Setting the compiler to gcc-9 etc. is done in the MEX-build script, although that needs sudo as well, 
 # so that the system setup of the user does not get modified too much
 
+# OUTDATED: Sometimes, which matlab does not return the correct path
 # Get the path to your Matlab executable (does not work without -i, need user because PATH is not set for sudo)
-MATLAB_PATH=$(su $real_user which matlab; exit)
-MATLAB_PATH=${MATLAB_PATH//'//'/'/'} # Get rid of double-// in the path, which may occur when using sudo -i
-if [[ -z "${MATLAB_PATH// }" ]]
+# MATLAB_PATH=$(su $real_user which matlab; exit)
+# MATLAB_PATH=${MATLAB_PATH//'//'/'/'} # Get rid of double-// in the path, which may occur when using sudo -i
+
+# The user needs to specify the path to the Matlab installation manually
+read -p 'To support our libraries in the eProsima MEX-Files, Matlab needs to be modified to use the standard C++ library of your system. Please enter the absolute installation path of Matlab (e.g. /opt/MATLAB/R2021a/): ' MATLAB_PATH
+#check, if a path to Matlab was entered
+while [ -z "$MATLAB_PATH" ]; do
+        echo "No Matlab path was entered, please try again"
+        read MATLAB_PATH
+done
+
+if ! [[ -d "$MATLAB_PATH" ]]
 then
     echo "WARNING: MATLAB is not installed / on your path. If you install it later on, you must run this script (matlab_setup.sh) manually again!"
 else
-    # Adapt the path to be the path to matlab (cut away /bin/matlab), then expand it to point to the path of the lib to replace
-    MATLAB_PATH=${MATLAB_PATH%/*}
-    MATLAB_PATH=${MATLAB_PATH%/*}
+    echo "Found $MATLAB_PATH"
+
+    # Adapt the path to be the path to to the lib to replace
     MATLAB_LIBSTDCPP_PATH="$MATLAB_PATH/sys/os/glnxa64"
 
     echo "Now going into $MATLAB_LIBSTDCPP_PATH to replace Matlab's libstdc++ file with the system version..."
