d5a66eec5ff24473929a2b8b460e8ac9dc40c8de
Leon Rabanus <leon.rabanus@rwth-aachen.de>
2022-01-07 17:21:19 +0100

    Tried to use the content filter / own debug filter

--- cpm_lib/test/test_VehicleIDFilteredTopic.cpp
+++ cpm_lib/test/test_VehicleIDFilteredTopic.cpp
@@ -6,6 +6,9 @@
 #include "cpm/Writer.hpp"
 #include <unistd.h>
 
+// Just for debugging
+#include "cpm/DebugFilter.hpp"
+
 /**
  * \test Tests VehicleIDFilteredTopic
  * 
@@ -17,86 +20,95 @@ TEST_CASE("VehicleIDFilteredTopic") {
     // One writer for all vehicle state test packages
     cpm::Writer<VehicleStatePubSubType> writer_vehicleState("my_topic_name");
 
-    // Reader for state packages with these IDs
-    cpm::Reader<VehicleStatePubSubType> reader_vehicle11("my_topic_name", 11);
-    cpm::Reader<VehicleStatePubSubType> reader_vehicle42("my_topic_name", 42);
-
-    //It usually takes some time for all instances to see each other - wait until then
-        std::cout << "Waiting for DDS entity match in VehicleIDFilteredTopic test" << std::endl << "\t";
-        bool wait = true;
-        while (wait)
-        {
-            usleep(10000); //Wait 10ms
-            std::cout << "." << std::flush;
-
-            if  (writer_vehicleState.matched_subscriptions_size() > 0 && 
-                    reader_vehicle11.matched_publications_size() >=1 && 
-                    reader_vehicle42.matched_publications_size() >= 1
-                )
-                wait = false;
-        }
-        std::cout << std::endl;
-
-    // send state packages with different IDs, also those that should be ignored
-    {
-        VehicleState vehicleState;
-        vehicleState.odometer_distance(123);
-        vehicleState.vehicle_id(3);
-        writer_vehicleState.write(vehicleState);
-    }
+    // Register debug filter before using it in the constructor of the readers
+    // Debugging, as the default filter does not work: Implement own one just for tests
+    DebugFilter debug_filter;
+    cpm::ParticipantSingleton::Instance()->register_content_filter_factory("debug_filter", &debug_filter);
 
     {
-        VehicleState vehicleState;
-        vehicleState.odometer_distance(2);
-        vehicleState.vehicle_id(42);
-        writer_vehicleState.write(vehicleState);
-    }
+        // Reader for state packages with these IDs
+        cpm::Reader<VehicleStatePubSubType> reader_vehicle11("my_topic_name", 11);
+        cpm::Reader<VehicleStatePubSubType> reader_vehicle42("my_topic_name", 42);
 
-    {
-        VehicleState vehicleState;
-        vehicleState.odometer_distance(3);
-        vehicleState.vehicle_id(11);
-        writer_vehicleState.write(vehicleState);
-    }
-
-    {
-        VehicleState vehicleState;
-        vehicleState.odometer_distance(6);
-        vehicleState.vehicle_id(42);
-        writer_vehicleState.write(vehicleState);
-    }
-
-    // wait for 'transmission' for up to 1 second
-        std::vector<VehicleState> reader_samples11;
-        std::vector<VehicleState> reader_samples42;
-        for (int i = 0; i < 10; ++i)
-        {
-            auto reader_samples11_dds = reader_vehicle11.get_all_samples();
-            auto reader_samples42_dds = reader_vehicle42.get_all_samples();
-
-            for (auto& sample : reader_samples11_dds)
+        //It usually takes some time for all instances to see each other - wait until then
+            std::cout << "Waiting for DDS entity match in VehicleIDFilteredTopic test" << std::endl << "\t";
+            bool wait = true;
+            while (wait)
             {
-                reader_samples11.push_back(sample);
-            }
-            for (auto& sample : reader_samples42_dds)
-            {
-                reader_samples42.push_back(sample);
-            }
+                usleep(10000); //Wait 10ms
+                std::cout << "." << std::flush;
 
-            //Abort early if condition is fulfilled, else wait and repeat read
-            if (reader_samples11.size() >=1 && reader_samples42.size() >= 2) break;
-            else usleep(100000);
+                if  (writer_vehicleState.matched_subscriptions_size() > 0 && 
+                        reader_vehicle11.matched_publications_size() >=1 && 
+                        reader_vehicle42.matched_publications_size() >= 1
+                    )
+                    wait = false;
+            }
+            std::cout << std::endl;
+
+        // send state packages with different IDs, also those that should be ignored
+        {
+            VehicleState vehicleState;
+            vehicleState.odometer_distance(123);
+            vehicleState.vehicle_id(3);
+            writer_vehicleState.write(vehicleState);
         }
 
-    REQUIRE(reader_samples11.size() == 1);
-    REQUIRE(reader_samples42.size() == 2);
+        {
+            VehicleState vehicleState;
+            vehicleState.odometer_distance(2);
+            vehicleState.vehicle_id(42);
+            writer_vehicleState.write(vehicleState);
+        }
 
-    REQUIRE(reader_samples42[0].vehicle_id() == 42);
-    REQUIRE(reader_samples42[0].odometer_distance() == 2);
+        {
+            VehicleState vehicleState;
+            vehicleState.odometer_distance(3);
+            vehicleState.vehicle_id(11);
+            writer_vehicleState.write(vehicleState);
+        }
 
-    REQUIRE(reader_samples42[1].vehicle_id() == 42);
-    REQUIRE(reader_samples42[1].odometer_distance() == 6);
+        {
+            VehicleState vehicleState;
+            vehicleState.odometer_distance(6);
+            vehicleState.vehicle_id(42);
+            writer_vehicleState.write(vehicleState);
+        }
 
-    REQUIRE(reader_samples11[0].vehicle_id() == 11);
-    REQUIRE(reader_samples11[0].odometer_distance() == 3);
+        // wait for 'transmission' for up to 1 second
+            std::vector<VehicleState> reader_samples11;
+            std::vector<VehicleState> reader_samples42;
+            for (int i = 0; i < 10; ++i)
+            {
+                auto reader_samples11_dds = reader_vehicle11.get_all_samples();
+                auto reader_samples42_dds = reader_vehicle42.get_all_samples();
+
+                for (auto& sample : reader_samples11_dds)
+                {
+                    reader_samples11.push_back(sample);
+                }
+                for (auto& sample : reader_samples42_dds)
+                {
+                    reader_samples42.push_back(sample);
+                }
+
+                //Abort early if condition is fulfilled, else wait and repeat read
+                if (reader_samples11.size() >=1 && reader_samples42.size() >= 2) break;
+                else usleep(100000);
+            }
+
+        REQUIRE(reader_samples11.size() == 1);
+        REQUIRE(reader_samples42.size() == 2);
+
+        REQUIRE(reader_samples42[0].vehicle_id() == 42);
+        REQUIRE(reader_samples42[0].odometer_distance() == 2);
+
+        REQUIRE(reader_samples42[1].vehicle_id() == 42);
+        REQUIRE(reader_samples42[1].odometer_distance() == 6);
+
+        REQUIRE(reader_samples11[0].vehicle_id() == 11);
+        REQUIRE(reader_samples11[0].odometer_distance() == 3);
+    }
+
+    cpm::ParticipantSingleton::Instance()->unregister_content_filter_factory("debug_filter");
 }
