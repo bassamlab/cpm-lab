6ab03baa7023c95053d27e23b854b467bcbcdcb7
Paul Kailer <paulkailer@outlook.de>
2023-05-15 15:40:30 +0200

    remove MATLAB example HLCs QP_central_routing QP_commonroad_target leader_follower and platoon

--- high_level_controller/examples/matlab/QP_central_routing/configVehicle.m
+++
@@ -1,9 +0,0 @@
-function cfg = configVehicle
-
-       cfg.Hu = 6; % Stellhorizont
-       cfg.Hp = 8; % (oberer) Praediktionshorizont
-       cfg.dt = 0.25; % Abtastzeit in Sekunden
-       cfg.dsafeVehicles = 0.15; % Sicherheitsabstand
-       cfg.minSpeed = 0.3; % minimale Geschwindigkeit
-       cfg.speed_target = 1; % Reisegeschwindigkeit
-end
